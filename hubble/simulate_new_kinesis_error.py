963408a52e8b78338d333fb72a9d71f61806ce4c
nickolaskraus-wf <nickolas.kraus@workiva.com>
2017-12-01 13:36:56 -0600

    simulate_new_kinesis_error.py now using process_errors endpoint
     - This is in lieu of hitting the /api/v1/hubble/incoming_errors
    endpoint, which is substantially slower

--- hubble/simulate_new_kinesis_error.py
+++ hubble/simulate_new_kinesis_error.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+# import argparse
 import base64
 import datetime
 import hashlib
@@ -10,10 +11,30 @@ import time
 import requests
 
 from settings import INSIGHT_STAGING_BASE_URL, INCOMING_KINESIS_ERRORS_PATH, \
-    LOCAL_COOKIE, STAGING_COOKIE, PROCESS_ERRORS_PATH
+    LOCAL_COOKIE, STAGING_COOKIE, TIME_FORMAT_NO_MICRO_SEC, SERVICE_SUFFIX
 
 
 def main():
+    # TODO: Add command line argument parser
+    # # configure command line argument parser
+    # parser = argparse.ArgumentParser(
+    #     description='Simulate a Kinesis error log on Insight.')
+    # group = parser.add_mutually_exclusive_group()
+    # # service to which the error is sent
+    # parser.add_argument('service',
+    #                     help='service to which the error is sent')
+    # # number of errors to send to Insight
+    # parser.add_argument('-n', '--number', type=int, default=1,
+    #                     help='number of errors to send')
+    # # specify if error should be new
+    # parser.add_argument('--new', action='store_true',
+    #                     help='send new error')
+    # # specify if error should be sent to local or staging Insight instance
+    # group.add_argument('-l', '--local', action='store_true',
+    #                     help='send to local instance')
+    # group.add_argument('-s', '--staging', action='store_true',
+    #                     help='send to staging instance')
+
     if len(sys.argv) < 3:
         usage()
         sys.exit(1)
@@ -23,20 +44,25 @@ def main():
     else:
         base_url = INSIGHT_STAGING_BASE_URL
 
+    # service is used for the log
     service = sys.argv[2]
+    # env is used for the process_errors request
+    env = get_service_env(service)
+    print service, env
     now = datetime.datetime.now()
     _time = now.strftime('%Y/%m/%d %H:%M:%S')
-    message = 'testing'
     hash = hashlib.sha256(str(now))
     context = str(datetime.datetime.now()) + '-' + str(hash.hexdigest())[0:7]
 
-    # TODO
+    # content of error log
     log = {
         "context": {},
         "exception": {},
         "level": "",
-        "message": message,
-        "metadata": {},
+        "message": context,
+        "metadata": {
+            "logger": "w_comments"
+        },
         "service": service,
         "time": _time,
         "version": ""
@@ -46,29 +72,52 @@ def main():
     raw_body = {'data': [encoded_data]}
     encoded_body = json.dumps(raw_body)
 
-    url = create_url(base_url, INCOMING_KINESIS_ERRORS_PATH)
+    # form data for POST /tasks/process_errors
+    utcnow = datetime.datetime.utcnow()
+    start_time = utcnow - datetime.timedelta(seconds=60)
+    end_time = utcnow + datetime.timedelta(seconds=60)
+    form_data = {
+        "start_time": start_time.strftime(TIME_FORMAT_NO_MICRO_SEC),
+        "end_time": end_time.strftime(TIME_FORMAT_NO_MICRO_SEC),
+        "env": env,
+        "source": "kinesis",
+        "should_check_lock": "True"
+    }
+
+    print end_time, start_time
+
+    process_errors = create_url(base_url, '/tasks/process_errors')
+    incoming_errors = create_url(base_url, INCOMING_KINESIS_ERRORS_PATH)
 
     headers = {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
+    }
+
+    form_headers = {
+        'X-AppEngine-QueueName': 'yes'
     }
 
     cookie = {
         "dev_appserver_login": LOCAL_COOKIE,
         "SACSID": STAGING_COOKIE}
 
-    # kick off process errors if local
-    if sys.argv[1] == '-l' or sys.argv[1] == '--local':
-        r = requests.get(base_url + PROCESS_ERRORS_PATH, cookies=cookie)
-        time.sleep(10)
-
-        if r.status_code != 200:
-            print 'An error has occurred. Status code: ' + str(r.status_code)
-            sys.exit(1)
-        else:
-            print 'Success! Status code: ' + str(r.status_code)
-
     # simulate kinesis error
-    r = requests.post(url, headers=headers, data=encoded_body, cookies=cookie)
+    r = requests.post(incoming_errors, headers=headers, data=encoded_body,
+                      cookies=cookie)
+
+    if r.status_code != 200:
+        print 'An error has occurred. Status code: ' + str(r.status_code)
+        # TODO: Use BeautifulSoup to parse error response
+        print r.text
+        sys.exit(1)
+    else:
+        print 'Success! Status code: ' + str(r.status_code)
+
+    time.sleep(1)
+
+    # simulate process_errors
+    r = requests.post(process_errors, headers=form_headers, data=form_data,
+                      cookies=cookie)
 
     if r.status_code != 200:
         print 'An error has occurred. Status code: ' + str(r.status_code)
@@ -87,6 +136,21 @@ def create_url(base, path, params=None):
         for key, value in params.iteritems():
             url += key + '=' + str(value) + '&'
         return url
+
+
+def get_service_env(service):
+    """
+    Get service env (Cerberus-prod => -prod).
+
+    :param service: name of the service
+    :type service: str
+    :return: env
+    :rtype: str
+    """
+    for suffix in SERVICE_SUFFIX:
+        if suffix in service:
+            return suffix
+    return ''
 
 
 def usage():
